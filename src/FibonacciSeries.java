ccfed32f042f6b8393722a6e81a4863a21393653
Kinotable <prof.darshdeep@gmail.com>
2024-09-22 19:35:46 +0530

    Assignment

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the nth term");
+        int nth = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.println("Fibonacci Series is as follows:");
+        for (int i = 1; i <= nth; i++) {
+            System.out.print(" " + a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
